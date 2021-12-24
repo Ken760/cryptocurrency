5c438dead35e9384a0dc8e099d599c2816a128a2
Ken760 <49761163+Ken760@users.noreply.github.com>
2021-12-24 11:43:09 +0300

    added api coingecko

--- coins/views.py
+++ coins/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def index(request):
+    url = 'https://api.coingecko.com/api/v3/coins/' \
+    'markets?vs_currency=rub&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    coins = requests.get(url).json()
+    return render(request, 'index.html', context={'coins': coins})
